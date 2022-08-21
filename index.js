7b6e8a1a08b0f0d242d6806098ef31cb369f44d9
MauriVaz <mvazquezcostal@gmail.com>
2022-08-21 03:18:30 +0200

    Finished

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ view = (num) => {
 };
 compute = () => {
   let x = document.getElementById('result').value;
-  let y = eval(x);
-  document.getElementById('result').value = y;
+  try {
+    let y = eval(x);
+    document.getElementById('result').value = y;
+  } catch (error) {
+    clr();
+  }
 };
+del = () => {
+  let x = document.getElementById('result').value;
+  let numS = x + '';
+  let find;
+  let operando;
+  array = x.split('');
+  for (let i = 0; i < array.length; i++) {
+    let x = array[i];
+    if (x === '/' || x === '+' || x === '-' || x === '*') {
+      find = i;
+    }
+  }
+  let num1 = numS.substring(0, find);
+  operando = array[find];
+  operando = parseInt(operando);
+  if (array.length === 1) {
+    clr();
+  } else {
+    document.getElementById('result').value = num1.charAt(0, find - 1);
+  }
+};
+
 clr = () => {
   document.getElementById('result').value = '';
 };
